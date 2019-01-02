3152a9dbe062d179db117e003a28d8743fc215e3
Nataliia <silvergirl@ukr.net>
2019-01-02 18:48:43 +0200

    lesson-12

--- source/bus/auth/saga/workers/signup.js
+++ source/bus/auth/saga/workers/signup.js
@@ -1,13 +1,11 @@
 //Core
 import { put, apply } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
 
 //Instruments
 import { api } from '../../../../REST';
 import { authActions } from '../../../auth/actions';
 import { uiActions } from '../../../ui/actions';
 import { profileActions } from '../../../profile/actions';
-
 
 export function* signup ({payload: userInfo}) {
     try {
